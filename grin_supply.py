c7af7f2ffe1eca204eb3c6bc430195e6c0894caa
Alex Waters <alex@waters.nyc>
2019-01-16 02:28:38 -0500

    readability improvement

--- grin_supply.py
+++ grin_supply.py
@@ -1,30 +1,32 @@
 from tabulate import tabulate
 
-#Grin Issuance Schedule
-#Second = 1 | Minute = 60 | Hour = 3,600 | Day = 86,400
-#Year = 31,536,000 | Decade = 315,360,000
+#Grin Issuance and Inflation Schedule
 
-g_sec = 1
-g_min = g_sec * 60
-g_hour = g_min * 60
-g_day = g_hour * 24
-g_year = g_day * 365
-g_decade = g_year * 10
+#Free Code available at: #https://github.com/alexwaters/sandbox/blob/master/grin_supply.py
 
-starting_year = 2019
-decade_years = 10
-num_decades = 5
+g_sec = 1             #Amount of grin per second/hour/day/year
+g_min = g_sec * 60    #60
+g_hour = g_min * 60   #3,600
+g_day = g_hour * 24   #86,400
+g_year = g_day * 365  #31,536,000
 
+decade = 10           #years in a decade
+
+starting_year = 2019  #genesis block year
+decades = 7           #number of decades to display
+
+#Get the stats for a single year
 def get_stat(year):
   assert (year >= starting_year)
   delta = (year - starting_year)
   supply = (delta+1)*g_year
   new = g_year
   inflation = new / supply
-  return {"Year": year, "Supply": supply, "New Coin": new, 
+  return {"Year": year, "Total Supply": supply, "New $GRIN": new, 
   "Inflation": '{:.2%}'.format(inflation)}
 
-for d in range(num_decades):
+#Get the stats for the specified number of decades
+for d in range(decades):
   sy = starting_year + (10 * d)
-  years = [get_stat((y + sy)) for y in range(decade_years)]
+  years = [get_stat((y + sy)) for y in range(decade)]
   print (tabulate(years, headers="keys") + "\n")
